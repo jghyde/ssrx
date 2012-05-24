fa0f2f6cea841f31f61ba6b189bc3c1714b78d17
David Fells <david@envisioninteractive.com>
2012-05-23 23:52:49 -0500

    Adding modules for drug content types, development tools, and searching / faceted search

--- docroot/sites/default/settings.php
+++ docroot/sites/default/settings.php
@@ -519,3 +519,4 @@ $conf['404_fast_html'] = '<html xmlns="http://www.w3.org/1999/xhtml"><head><titl
 if (file_exists('/var/www/site-php')) {
   require('/var/www/site-php/tom17/tom17-settings.inc');
 }
+@include('local.settings.php');
