7806969eacddbffaca3b27045cd7e40e17b92fa5
Joe Hyde <joeh@hydeinteractive.com>
2012-06-23 08:38:43 -0500

    adds address in words to gmap bubbles

--- docroot/sites/all/themes/ssrx/templates/location/location.tpl.php
+++ docroot/sites/all/themes/ssrx/templates/location/location.tpl.php
@@ -1,6 +1,23 @@
+<?php
+if (arg(0) != 'node') {
+  $i = 0;
+  // dpm(get_defined_vars());
+  // print out the street address:
+  print $location['street'] . '<br />';
+  if (!empty($location['additional'])) {
+    print $location['additional'];
+  }
+  print $location['city'] . ', ' . $location['province'] . ' ' . $location['postal_code'] . '<br />';
+  if (!empty($location['phone'])) {
+    print 'Phone: ' . $location['phone'] . '<br />';
+  }
+}
+?>
+<?php if (!empty($gmap)): ?>
 <div class="gmap">
   <?php print drupal_render($gmap); ?>
 </div>
+<?php endif; ?>
 <?php if (!empty($map_link)): ?>
 <div class="map-link">
   <?php print $map_link; ?>
